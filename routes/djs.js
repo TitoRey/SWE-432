88878554c252f24ee8018249138b5d07c21668ca
Tito Reynoso <rreynoso@solutionstreet.com>
2023-11-01 20:29:20 -0400

    Finished iteration 4

--- routes/djs.js
+++ routes/djs.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => { 
-  res.render('djs/index');
+  res.render('djs/index', { name: 'Tito'});
 });
 
 router.get('/lookup', (req, res) => {
-  res.render('djs/lookup');
+  res.render('djs/lookup', { name: 'Tito'});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Using Static Files
 // app.use(express.static("public"));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
+
 
 // Using Express Layouts
 app.use(expressLayouts);
@@ -16,5 +18,13 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 app.listen(3000);
 
+app.get('/', (req, res) => { 
+  res.redirect('/djs');
+});
+
 const djRouter = require('./routes/djs');
 app.use('/djs', djRouter);
+
+app.all('*', (req, res) => { 
+  res.status(404).render('404');
+}); 
